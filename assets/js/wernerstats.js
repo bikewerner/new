bb5ee6a90d97bcc5bf2920c55577b1f5ff149b58
bikewerner <muenchen-1990@web.de>
2025-02-28 00:10:00 +0100

    Update wernerstats.js

--- assets/js/wernerstats.js
+++ assets/js/wernerstats.js
@@ -85,41 +85,6 @@ window.onload = () => {
 
 
 
-
-/* alte Version -> Zuweisungen nicht korrekt */
-window.onload = () => {
-const url = "https://docs.google.com/spreadsheets/d/1mHrH9Og6JcrQZttzevMcsIC3CPmaxY9SZdTTM9wI8JY/gviz/tq?tqx=out:csv&range=W2:AA7";
-
-fetch(url)
-    .then(res => res.text())
-    .then(text => {
-        const rows = text.trim().split("\n").map(row => row.split(","));
-
-        // IDs in derselben Reihenfolge wie in der Tabelle
-        const ids = [
-            "WalzerBerg", "WCBerg", "WindelBerg", "WuBerg", "WurstBerg",
-            "WalzerSprint", "WCSprint", "WindelSprint", "WuSprint", "WurstSprint",
-            "WalzerTeilnahmen", "WCTeilnahmen", "WindelTeilnahmen", "WuTeilnahmen", "WurstTeilnahmen",
-            "WalzerKilometer", "WCKilometer", "WindelKilometer", "WuKilometer", "WurstKilometer",
-            "WalzerHoehenmeter", "WCHoehenmeter", "WindelHoehenmeter", "WuHoehenmeter", "WurstHoehenmeter",
-            "WalzerPlatten", "WCPlatten", "WindelPlatten", "WuPlatten", "WurstPlatten"
-        ];
-
-        // Daten zu den Elementen zuweisen
-        rows.forEach((row, rowIndex) => {
-            row.forEach((cell, colIndex) => {
-                const id = ids[rowIndex * row.length + colIndex]; 
-                if (id) {
-                    document.getElementById(id).innerText = cell.replace(/^"|"$/g, '');
-                }
-            });
-        });
-    })
-    .catch(error => console.error("Fehler beim Laden der Daten:", error));
-};
-
-
-
 window.onload = () => {
     const url = "https://docs.google.com/spreadsheets/d/1mHrH9Og6JcrQZttzevMcsIC3CPmaxY9SZdTTM9wI8JY/gviz/tq?tqx=out:csv&range=W2:AA7";
     
@@ -160,4 +125,3 @@ window.onload = () => {
     // Weitere Daten für die Tabelle laden
     fetchData(url);
 };
-
