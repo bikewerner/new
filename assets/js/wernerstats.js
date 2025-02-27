ffcee7481260b892cb37643c8a87864811e20cd2
bikewerner <muenchen-1990@web.de>
2025-02-27 23:48:33 +0100

    Update wernerstats.js

--- assets/js/wernerstats.js
+++ assets/js/wernerstats.js
@@ -9,7 +9,7 @@ window.onload = () => {
     // Daten für AnzahlTouren abrufen
     fetchData("https://docs.google.com/spreadsheets/d/1mHrH9Og6JcrQZttzevMcsIC3CPmaxY9SZdTTM9wI8JY/gviz/tq?tqx=out:csv&range=B1", "AnzahlTouren");
 
-    // Alle anderen Daten auf einmal abrufen
+    // Daten für den relevanten Bereich W2:AA7 abrufen
     const url = "https://docs.google.com/spreadsheets/d/1mHrH9Og6JcrQZttzevMcsIC3CPmaxY9SZdTTM9wI8JY/gviz/tq?tqx=out:csv&range=W2:AA7";
 
     fetch(url)
@@ -20,41 +20,41 @@ window.onload = () => {
 
             // Mapping der Zellen basierend auf der Tabelle
             const data = {
-                "WalzerBerg": rows[1][0],
-                "WCBerg": rows[1][1],
-                "WindelBerg": rows[1][2],
-                "WuBerg": rows[1][3],
-                "WurstBerg": rows[1][4],
+                "WalzerBerg": rows[0][0],
+                "WCBerg": rows[0][1],
+                "WindelBerg": rows[0][2],
+                "WuBerg": rows[0][3],
+                "WurstBerg": rows[0][4],
                 
-                "WalzerSprint": rows[2][0],
-                "WCSprint": rows[2][1],
-                "WindelSprint": rows[2][2],
-                "WuSprint": rows[2][3],
-                "WurstSprint": rows[2][4],
+                "WalzerSprint": rows[1][0],
+                "WCSprint": rows[1][1],
+                "WindelSprint": rows[1][2],
+                "WuSprint": rows[1][3],
+                "WurstSprint": rows[1][4],
                 
-                "WalzerTeilnahmen": rows[3][0],
-                "WCTeilnahmen": rows[3][1],
-                "WindelTeilnahmen": rows[3][2],
-                "WuTeilnahmen": rows[3][3],
-                "WurstTeilnahmen": rows[3][4],
+                "WalzerTeilnahmen": rows[2][0],
+                "WCTeilnahmen": rows[2][1],
+                "WindelTeilnahmen": rows[2][2],
+                "WuTeilnahmen": rows[2][3],
+                "WurstTeilnahmen": rows[2][4],
                 
-                "WalzerKilometer": rows[4][0],
-                "WCKilometer": rows[4][1],
-                "WindelKilometer": rows[4][2],
-                "WuKilometer": rows[4][3],
-                "WurstKilometer": rows[4][4],
+                "WalzerKilometer": rows[3][0],
+                "WCKilometer": rows[3][1],
+                "WindelKilometer": rows[3][2],
+                "WuKilometer": rows[3][3],
+                "WurstKilometer": rows[3][4],
                 
-                "WalzerHoehenmeter": rows[5][0],
-                "WCHoehenmeter": rows[5][1],
-                "WindelHoehenmeter": rows[5][2],
-                "WuHoehenmeter": rows[5][3],
-                "WurstHoehenmeter": rows[5][4],
+                "WalzerHoehenmeter": rows[4][0],
+                "WCHoehenmeter": rows[4][1],
+                "WindelHoehenmeter": rows[4][2],
+                "WuHoehenmeter": rows[4][3],
+                "WurstHoehenmeter": rows[4][4],
                 
-                "WalzerPlatten": rows[6][0],
-                "WCPlatten": rows[6][1],
-                "WindelPlatten": rows[6][2],
-                "WuPlatten": rows[6][3],
-                "WurstPlatten": rows[6][4],
+                "WalzerPlatten": rows[5][0],
+                "WCPlatten": rows[5][1],
+                "WindelPlatten": rows[5][2],
+                "WuPlatten": rows[5][3],
+                "WurstPlatten": rows[5][4],
             };
 
             // Werte zu den entsprechenden HTML-Elementen zuweisen
