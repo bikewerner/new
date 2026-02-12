3a2dbce7c932a6998391b4d366b585f3b7c5f0ff
bikewerner <muenchen-1990@web.de>
2026-02-12 15:21:24 +0100

    Update nav-loader.js

--- assets/js/nav-loader.js
+++ assets/js/nav-loader.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // Lädt die zentrale nav.html
     $("#nav").load("nav.html", function() {
         
-        // 1. Desktop Dropdowns (wie in fuessen.html)
+        // 1. Desktop Dropdowns initialisieren
         $('#nav > ul').dropotron({
             mode: 'fade',
             noOpenerFade: true,
@@ -10,30 +10,28 @@ $(document).ready(function() {
             alignment: 'center'
         });
 
-        // 2. Mobile Menü (Panel) reparieren
+        // 2. Mobile Ansicht (Panel) gewaltsam füllen
         var $navPanel = $('#navPanel');
         if ($navPanel.length > 0) {
-            var $mobileNav = $navPanel.find('nav');
+            // Wir suchen das innere Nav-Element im Panel
+            var $panelNav = $navPanel.find('nav');
             
-            // Kopiert die Liste in das mobile Panel
-            $mobileNav.html($('#nav').html());
+            // Falls das Panel schon existiert, leeren wir es und kopieren unser geladenes Menü hinein
+            $panelNav.empty();
+            $panelNav.html($('#nav').html());
 
-            // FORMATIERUNG WIEDERHERSTELLEN:
-            // Wir gehen jeden Link durch und weisen ihm die "Tiefe" (depth) zu
-            $mobileNav.find('a').each(function() {
+            // Jetzt verpassen wir jedem Link die Tiefe (depth) für das Styling
+            $panelNav.find('a').each(function() {
                 var $a = $(this);
-                // Berechnet, wie tief der Link im Menü sitzt
                 var depth = $a.parents('ul').length - 1;
-                
-                // Fügt die Klassen hinzu, die das CSS (main.css) erwartet
                 $a.addClass('link depth-' + depth);
                 
-                // Falls es ein Hauptpunkt (depth 0) ist, bekommt er das weiße Design
-                // Falls es ein Unterpunkt (depth 1) ist, wird er eingerückt und grau
+                // Entferne mobile Event-Handler, die das Menü einklappen könnten
+                $a.off('click'); 
             });
         }
         
-        // 3. Aktiven Link markieren
+        // 3. Aktive Seite markieren
         var path = window.location.pathname.split("/").pop() || 'index.html';
         $('a[href="' + path + '"]').closest('li').addClass('current');
     });
