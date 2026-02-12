9b573c3cc6acf94f5376efab39cf5f5a784ccc16
bikewerner <muenchen-1990@web.de>
2026-02-12 15:28:07 +0100

    Update nav-loader.js

--- assets/js/nav-loader.js
+++ assets/js/nav-loader.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // Lädt die zentrale nav.html
     $("#nav").load("nav.html", function() {
         
-        // 1. Desktop Dropdowns initialisieren
+        // 1. Desktop Dropdowns (wie im Original)
         $('#nav > ul').dropotron({
             mode: 'fade',
             noOpenerFade: true,
@@ -10,28 +10,37 @@ $(document).ready(function() {
             alignment: 'center'
         });
 
-        // 2. Mobile Ansicht (Panel) gewaltsam füllen
-        var $navPanel = $('#navPanel');
-        if ($navPanel.length > 0) {
-            // Wir suchen das innere Nav-Element im Panel
-            var $panelNav = $navPanel.find('nav');
-            
-            // Falls das Panel schon existiert, leeren wir es und kopieren unser geladenes Menü hinein
-            $panelNav.empty();
-            $panelNav.html($('#nav').html());
-
-            // Jetzt verpassen wir jedem Link die Tiefe (depth) für das Styling
-            $panelNav.find('a').each(function() {
-                var $a = $(this);
-                var depth = $a.parents('ul').length - 1;
-                $a.addClass('link depth-' + depth);
+        // 2. Mobile Menü (Panel) komplett neu aufbauen
+        // Wir warten 100ms, um sicherzugehen, dass das Template fertig ist
+        setTimeout(function() {
+            var $navPanel = $('#navPanel');
+            if ($navPanel.length > 0) {
+                var $mobileNav = $navPanel.find('nav');
                 
-                // Entferne mobile Event-Handler, die das Menü einklappen könnten
-                $a.off('click'); 
-            });
-        }
-        
-        // 3. Aktive Seite markieren
+                // Wir leeren das mobile Panel komplett
+                $mobileNav.empty();
+                
+                // Wir kopieren das komplette Menü aus der geladenen nav.html hinein
+                var $menuClone = $('#nav > ul').clone();
+                $mobileNav.append($menuClone);
+
+                // Jetzt vergeben wir die Klassen für Einrückung und Design (depth-0, depth-1)
+                $mobileNav.find('a').each(function() {
+                    var $a = $(this);
+                    var depth = $a.parents('ul').length - 1;
+                    
+                    // Wir geben JEDEM Link die Klasse "link" und seine Tiefe
+                    $a.attr('class', 'link depth-' + depth);
+                    
+                    // Damit das Menü nicht bei Klick auf "Geplante Touren" zuklappt (da es ein # Link ist)
+                    if ($a.attr('href') == '#') {
+                        $a.on('click', function(e) { e.preventDefault(); });
+                    }
+                });
+            }
+        }, 100);
+
+        // 3. Aktiven Link markieren
         var path = window.location.pathname.split("/").pop() || 'index.html';
         $('a[href="' + path + '"]').closest('li').addClass('current');
     });
